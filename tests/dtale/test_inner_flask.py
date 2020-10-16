f8a69a70fdf3f6ca7d20f9470d55357ad045aeef
Andrew Schonfeld <andrew.schonfeld1@gmail.com>
2020-10-15 22:20:43 -0400

    https://github.com/man-group/dtale/issues/282: additional exception handling for overriding routes

--- tests/dtale/test_inner_flask.py
+++ tests/dtale/test_inner_flask.py
@@ -1,18 +1,50 @@
+import mock
 import pytest
+
+from six import PY3
 
 from dtale.app import build_app
 
-app = build_app(reaper_on=False)
-
-
-@app.route("/")
-def hello_world():
-    return "hello world"
+if PY3:
+    from contextlib import ExitStack
+else:
+    from contextlib2 import ExitStack
 
 
 @pytest.mark.unit
-def test_processes():
+def test_overriden_route():
+    app = build_app(reaper_on=False)
+
+    @app.route("/")
+    def hello_world():
+        return "hello world"
+
     app._override_routes()
     with app.test_client() as c:
         resp = c.get("/")
         assert resp.data == b"hello world"
+
+
+@pytest.mark.unit
+def test_failed_override():
+    with ExitStack() as stack:
+        stack.enter_context(
+            mock.patch(
+                "dtale.app.contains_route",
+                mock.Mock(side_effect=Exception("test error")),
+            )
+        )
+        app = build_app(reaper_on=False)
+
+        @app.route("/")
+        def hello_world():
+            return "hello world"
+
+        app._override_routes()
+
+        with app.test_client() as c:
+            resp = c.get("/")
+            assert resp.status_code == 302
+            assert resp.location == "http://localhost:{}/dtale/popup/upload".format(
+                c.port
+            )
