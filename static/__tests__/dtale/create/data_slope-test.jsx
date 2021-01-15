4c05f0cb590d27e92162e7532046e9eb96b37cd6
Andrew Schonfeld <andrew.schonfeld1@gmail.com>
2021-01-14 23:24:33 -0500

    https://github.com/man-group/dtale/issues/330: data slope column builder

---
+++ static/__tests__/dtale/create/data_slope-test.jsx
@@ -0,0 +1,111 @@
+import { mount } from "enzyme";
+import React from "react";
+import { Provider } from "react-redux";
+import Select from "react-select";
+
+import { expect, it } from "@jest/globals";
+
+import mockPopsicle from "../../MockPopsicle";
+import reduxUtils from "../../redux-test-utils";
+import { buildInnerHTML, clickMainMenuButton, tick, tickUpdate, withGlobalJquery } from "../../test-utils";
+import { clickBuilder } from "./create-test-utils";
+
+const originalOffsetHeight = Object.getOwnPropertyDescriptor(HTMLElement.prototype, "offsetHeight");
+const originalOffsetWidth = Object.getOwnPropertyDescriptor(HTMLElement.prototype, "offsetWidth");
+const originalInnerWidth = Object.getOwnPropertyDescriptor(HTMLElement.prototype, "innerWidth");
+const originalInnerHeight = Object.getOwnPropertyDescriptor(HTMLElement.prototype, "innerHeight");
+
+function submit(res) {
+  res.find("div.modal-footer").first().find("button").first().simulate("click");
+}
+
+describe("DataViewer tests", () => {
+  let result, CreateColumn, CreateDataSlope;
+
+  beforeAll(() => {
+    Object.defineProperty(HTMLElement.prototype, "offsetHeight", {
+      configurable: true,
+      value: 500,
+    });
+    Object.defineProperty(HTMLElement.prototype, "offsetWidth", {
+      configurable: true,
+      value: 500,
+    });
+    Object.defineProperty(window, "innerWidth", {
+      configurable: true,
+      value: 1205,
+    });
+    Object.defineProperty(window, "innerHeight", {
+      configurable: true,
+      value: 775,
+    });
+
+    const mockBuildLibs = withGlobalJquery(() =>
+      mockPopsicle.mock(url => {
+        const { urlFetcher } = require("../../redux-test-utils").default;
+        return urlFetcher(url);
+      })
+    );
+
+    const mockChartUtils = withGlobalJquery(() => (ctx, cfg) => {
+      const chartCfg = { ctx, cfg, data: cfg.data, destroyed: false };
+      chartCfg.destroy = () => (chartCfg.destroyed = true);
+      chartCfg.getElementsAtXAxis = _evt => [{ _index: 0 }];
+      chartCfg.getElementAtEvent = _evt => [{ _datasetIndex: 0, _index: 0, _chart: { config: cfg, data: cfg.data } }];
+      return chartCfg;
+    });
+
+    jest.mock("popsicle", () => mockBuildLibs);
+    jest.mock("chart.js", () => mockChartUtils);
+    jest.mock("chartjs-plugin-zoom", () => ({}));
+    jest.mock("chartjs-chart-box-and-violin-plot/build/Chart.BoxPlot.js", () => ({}));
+  });
+
+  beforeEach(async () => {
+    CreateColumn = require("../../../popups/create/CreateColumn").ReactCreateColumn;
+    CreateDataSlope = require("../../../popups/create/CreateDataSlope").CreateDataSlope;
+    const { DataViewer } = require("../../../dtale/DataViewer");
+
+    const store = reduxUtils.createDtaleStore();
+    buildInnerHTML({ settings: "" }, store);
+    result = mount(
+      <Provider store={store}>
+        <DataViewer />
+      </Provider>,
+      { attachTo: document.getElementById("content") }
+    );
+    await tick();
+    clickMainMenuButton(result, "Build Column");
+    await tickUpdate(result);
+    clickBuilder(result, "Z-Score Normalize");
+  });
+
+  afterAll(() => {
+    Object.defineProperty(HTMLElement.prototype, "offsetHeight", originalOffsetHeight);
+    Object.defineProperty(HTMLElement.prototype, "offsetWidth", originalOffsetWidth);
+    Object.defineProperty(window, "innerWidth", originalInnerWidth);
+    Object.defineProperty(window, "innerHeight", originalInnerHeight);
+  });
+
+  it("DataViewer: build z-score normalize column", async () => {
+    expect(result.find(CreateDataSlope).length).toBe(1);
+    result.find(CreateDataSlope).find(Select).first().instance().onChange({ value: "col1" });
+    result.update();
+    submit(result);
+    await tick();
+    expect(result.find(CreateColumn).instance().state.cfg).toEqual({
+      col: "col1",
+    });
+    expect(result.find(CreateColumn).instance().state.name).toBe("col1_data_slope");
+  });
+
+  it("DataViewer: build z-score normalize cfg validation", () => {
+    const { validateDataSlopeCfg } = require("../../../popups/create/CreateDataSlope");
+    expect(validateDataSlopeCfg({})).toBe("Please select a column!");
+    expect(
+      validateDataSlopeCfg({
+        col: "col1",
+      })
+    ).toBeNull();
+  });
+});
