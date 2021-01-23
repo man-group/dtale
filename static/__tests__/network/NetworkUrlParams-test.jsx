1e921c7fd02c35a9b31f02d6eb3f7674b1734ee4
Andrew Schonfeld <andrew.schonfeld1@gmail.com>
2021-01-23 11:05:29 -0500

    Node tooltips and URL history building for Network Viewer

---
+++ static/__tests__/network/NetworkUrlParams-test.jsx
@@ -0,0 +1,62 @@
+import { shallow } from "enzyme";
+import React from "react";
+
+import { expect, it } from "@jest/globals";
+
+import actions from "../../actions/dtale";
+import NetworkUrlParams from "../../network/NetworkUrlParams";
+
+describe("NetworkUrlParams", () => {
+  const { onpopstate } = window;
+  const { history } = global;
+  let result, propagateState, getParamsSpy;
+  const params = { to: "to", from: "from", group: "group", weight: "weight" };
+
+  beforeAll(() => {
+    delete window.onpopstate;
+    window.onpopstate = jest.fn();
+  });
+
+  beforeEach(() => {
+    propagateState = jest.fn();
+    getParamsSpy = jest.spyOn(actions, "getParams");
+    result = shallow(<NetworkUrlParams params={undefined} propagateState={propagateState} />);
+  });
+
+  afterEach(() => {
+    getParamsSpy.mockRestore();
+  });
+
+  afterAll(() => {
+    window.onpopstate = onpopstate;
+    Object.defineProperty(global, "history", history);
+  });
+
+  it("renders successfully", () => {
+    expect(result.html()).toBeNull();
+  });
+
+  it("correctly updates history", () => {
+    const pushState = jest.fn();
+    Object.defineProperty(global.history, "pushState", {
+      value: pushState,
+    });
+    getParamsSpy.mockReturnValue({});
+    result.setProps({ params });
+    expect(pushState).toHaveBeenLastCalledWith({}, "", "?to=to&from=from&group=group&weight=weight");
+    window.onpopstate();
+    expect(propagateState).toHaveBeenLastCalledWith({
+      to: null,
+      from: null,
+      group: null,
+      weight: null,
+    });
+    pushState.mockClear();
+    result.setProps({ params });
+    expect(pushState).not.toHaveBeenCalled();
+    getParamsSpy.mockReturnValue(params);
+    result.setProps({ params: { ...params, test: "blah" } });
+    expect(pushState).not.toHaveBeenCalled();
+    result.unmount();
+  });
+});
--- static/network/networkUtils.js
+++ static/network/networkUtils.js
@@ -1,3 +1,5 @@
+import _ from "lodash";
+
 export const BASE_LAYOUT = { randomSeed: 2 };
 export const OPTIONS = {
   nodes: {
@@ -44,4 +46,43 @@ export const resetNode = (node, color = undefined) => {
     node.label = node.hiddenLabel;
     node.hiddenLabel = undefined;
   }
+};
+
+export const neighborhoodHighlight = ({ allNodes, highlightActive }, network, { nodes }) => {
+  const nodesDataset = network.body.data.nodes;
+  // if something is selected:
+  if (nodes.length > 0) {
+    highlightActive = true;
+    const selectedNodeId = nodes[0];
+
+    // mark all nodes as hard to read.
+    _.forEach(allNodes, node => {
+      node.color = "rgba(200,200,200,0.5)";
+      if (node.hiddenLabel === undefined) {
+        node.hiddenLabel = node.label;
+        node.label = undefined;
+      }
+    });
+    const connectedNodes = network.getConnectedNodes(selectedNodeId);
+    let allConnectedNodes = [];
+
+    // get the second degree nodes
+    _.forEach(connectedNodes, node => (allConnectedNodes = allConnectedNodes.concat(network.getConnectedNodes(node))));
+
+    // all second degree nodes get a different color and their label back
+    _.forEach(allConnectedNodes, connectedNode => resetNode(allNodes[connectedNode], "rgba(150,150,150,0.75)"));
+
+    // all first degree nodes get their own color and their label back
+    _.forEach(connectedNodes, connectedNode => resetNode(allNodes[connectedNode]));
+
+    // the main node gets its own color and its label back.
+    resetNode(allNodes[selectedNodeId]);
+  } else if (highlightActive === true) {
+    // reset all nodes
+    _.forEach(allNodes, node => resetNode(node));
+    highlightActive = false;
+  }
+
+  nodesDataset.update(_.values(allNodes));
+  return highlightActive;
 };
