eca1a64991326d53eb0cb4cea4586807b62fe996
Andrew Schonfeld <andrew.schonfeld1@gmail.com>
2022-03-13 22:19:39 -0400

    bumped version numbers to 2.1.0

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ class PyTest(TestCommand):
 
 setup(
     name="dtale",
-    version="2.0.0",
+    version="2.1.0",
     author="MAN Alpha Technology",
     author_email="ManAlphaTech@man.com",
     description="Web Client for Visualizing Pandas Objects",
