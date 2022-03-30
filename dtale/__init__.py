f63796f32a1924d75daa5e24c0f23e533eaa8857
Andrew Schonfeld <andrew.schonfeld1@gmail.com>
2022-03-30 16:51:49 -0400

    https://github.com/man-group/dtale/issues/653: added "__version__" entry to dtale module

--- dtale/__init__.py
+++ dtale/__init__.py
@@ -9,6 +9,7 @@ dtale = Blueprint("dtale", __name__, url_prefix="/dtale")
 # flake8: NOQA
 from dtale.app import show, get_instance, instances, offline_chart  # isort:skip
 from dtale.cli.loaders import LOADERS  # isort:skip
+from dtale.cli.clickutils import retrieve_meta_info_and_version
 from dtale.global_state import update_id  # isort:skip
 
 ALLOW_CELL_EDITS = True
@@ -18,3 +19,6 @@ GITHUB_FORK = False
 for loader_name, loader in LOADERS.items():
     if hasattr(loader, "show_loader"):
         globals()["show_{}".format(loader_name)] = loader.show_loader
+
+
+__version__ = retrieve_meta_info_and_version("dtale")[1]
