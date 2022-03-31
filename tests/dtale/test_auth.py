65455fc9658a128921a77affbe162b17bd02431e
Andrew Schonfeld <andrew.schonfeld1@gmail.com>
2022-03-31 11:18:12 -0400

    Fix black & flask upgrades

--- tests/dtale/test_auth.py
+++ tests/dtale/test_auth.py
@@ -60,17 +60,17 @@ def test_login():
 
             resp = c.get("/dtale/main/{}".format(c.port))
             assert resp.status_code == 302
-            assert resp.location == "http://localhost:{}/login".format(c.port)
+            assert "http://localhost:{}/login".format(c.port).endswith(resp.location)
 
             resp = c.post("/login", data=dict(username="foo", password="bar"))
-            assert resp.location == "http://localhost:{}/dtale/main/{}".format(
-                c.port, c.port
+            assert "http://localhost:{}/dtale/main/{}".format(c.port, c.port).endswith(
+                resp.location
             )
             assert mock_session["logged_in"]
             assert mock_session["username"] == "foo"
 
             resp = c.get("/logout")
-            assert resp.location == "http://localhost:{}/login".format(c.port)
+            assert "http://localhost:{}/login".format(c.port).endswith(resp.location)
             assert mock_session.get("logged_in") is None
 
 
