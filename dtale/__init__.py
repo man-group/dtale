e0bcf10c03055a1f000ac007f80c805d1f4ca6fd
Andrew Schonfeld <andrew.schonfeld1@gmail.com>
2022-05-28 21:48:58 -0400

    https://github.com/man-group/dtale/issues/668: update warning filtering to only filter dash_html_components message

--- dtale/__init__.py
+++ dtale/__init__.py
@@ -2,23 +2,29 @@ import warnings
 
 from flask import Blueprint
 
-warnings.filterwarnings("ignore")
+with warnings.catch_warnings():
+    warnings.filterwarnings(
+        "ignore",
+        (
+            "\nThe dash_html_components package is deprecated. Please replace"
+            "\n`import dash_html_components as html` with `from dash import html`"
+        ),
+    )
 
-dtale = Blueprint("dtale", __name__, url_prefix="/dtale")
+    dtale = Blueprint("dtale", __name__, url_prefix="/dtale")
 
-# flake8: NOQA
-from dtale.app import show, get_instance, instances, offline_chart  # isort:skip
-from dtale.cli.loaders import LOADERS  # isort:skip
-from dtale.cli.clickutils import retrieve_meta_info_and_version
-from dtale.global_state import update_id  # isort:skip
+    # flake8: NOQA
+    from dtale.app import show, get_instance, instances, offline_chart  # isort:skip
+    from dtale.cli.loaders import LOADERS  # isort:skip
+    from dtale.cli.clickutils import retrieve_meta_info_and_version
+    from dtale.global_state import update_id  # isort:skip
 
-ALLOW_CELL_EDITS = True
-HIDE_SHUTDOWN = False
-GITHUB_FORK = False
+    ALLOW_CELL_EDITS = True
+    HIDE_SHUTDOWN = False
+    GITHUB_FORK = False
 
-for loader_name, loader in LOADERS.items():
-    if hasattr(loader, "show_loader"):
-        globals()["show_{}".format(loader_name)] = loader.show_loader
+    for loader_name, loader in LOADERS.items():
+        if hasattr(loader, "show_loader"):
+            globals()["show_{}".format(loader_name)] = loader.show_loader
 
-
-__version__ = retrieve_meta_info_and_version("dtale")[1]
+    __version__ = retrieve_meta_info_and_version("dtale")[1]
