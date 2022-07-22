1d879c8499820e6a3aac170a4aa6588d4233a368
Maciej Matraszek <matraszek.maciej@gmail.com>
2022-07-22 21:00:47 +0200

    Fix filtering strings with special characters

--- tests/dtale/test_column_filters.py
+++ tests/dtale/test_column_filters.py
@@ -32,35 +32,48 @@ def test_string():
             return query
         return query.replace("`", "")
 
-    df = pd.DataFrame(dict(foo=["AAA", "aaa", "ABB", "ACC"]))
+    df = pd.DataFrame(dict(foo=["AAA", "aaa", "ABB", "ACC", "\'\"{[\\AA"]))
 
     cfg = dict(action="equals", operand="=", value=["AAA"])
 
     assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 1
     cfg["operand"] = "ne"
-    assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 3
+    assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 4
 
     cfg["value"] = ["AAA", "aaa"]
     cfg["operand"] = "="
     assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 2
+    cfg["value"] = ["\'\"{[\\AA"]
+    assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 1
+    cfg["value"] = ["aaa", "\'\"{[\\AA"]
+    assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 2
+
+    cfg["raw"] = "'"
+    cfg["action"] = "startswith"
+    assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 1
 
     cfg["raw"] = "AA"
     cfg["action"] = "startswith"
     assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 2
     cfg["operand"] = "ne"
-    assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 2
+    assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 3
     cfg["action"] = "endswith"
     cfg["operand"] = "="
-    assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 2
+    assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 3
     cfg["caseSensitive"] = True
-    assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 1
+    assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 2
 
     cfg["action"] = "contains"
     cfg["caseSensitive"] = False
+    cfg["raw"] = "\'\""
+    assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 1
+    cfg["raw"] = "[\\" # str.contains parses regex
+    assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 1
+
     cfg["raw"] = "A"
-    assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 4
+    assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 5
     cfg["caseSensitive"] = True
-    assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 3
+    assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 4
     cfg["raw"] = "D"
     assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 0
 
