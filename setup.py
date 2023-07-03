2f6f0aa46a20c6e148c8e4449deb848a161af39e
Andrew Schonfeld (Boston) <andrew.schonfeld@man.com>
2023-07-03 13:09:47 -0400

    Added python11 support

--- setup.py
+++ setup.py
@@ -108,6 +108,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     cmdclass={"test": PyTest},
     packages=find_packages(exclude=["tests*", "script*"]),
