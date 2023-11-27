5e31da59f4c45a8dc2881bac1179323e2ce60c30
Andrew Schonfeld (Boston) <andrew.schonfeld@man.com>
2023-11-27 12:22:25 -0500

    https://github.com/man-group/dtale/issues/827: allowing user's to override the host used by Flask.run

--- tests/dtale/test_column_filters.py
+++ tests/dtale/test_column_filters.py
@@ -5,6 +5,7 @@ import pytest
 
 from dtale.column_filters import DateFilter, NumericFilter, StringFilter
 from dtale.pandas_util import check_pandas_version
+from dtale.query import run_query
 
 
 @pytest.mark.unit
@@ -46,63 +47,70 @@ def test_string():
 
     cfg = dict(action="equals", operand="=", value=["AAA"], type="string")
 
-    assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 1
+    assert len(run_query(df, build_query(StringFilter("foo", "S", cfg)))) == 1
     cfg["operand"] = "ne"
-    assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 4
+    assert len(run_query(df, build_query(StringFilter("foo", "S", cfg)))) == 4
 
     cfg["value"] = ["AAA", "aaa"]
     cfg["operand"] = "="
-    assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 2
+    assert len(run_query(df, build_query(StringFilter("foo", "S", cfg)))) == 2
     cfg["value"] = ["'\"{[\\AA"]
-    assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 1
+    assert len(run_query(df, build_query(StringFilter("foo", "S", cfg)))) == 1
     cfg["value"] = ["aaa", "'\"{[\\AA"]
-    assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 2
+    assert len(run_query(df, build_query(StringFilter("foo", "S", cfg)))) == 2
 
     if is_pandas25:
         cfg["raw"] = "'"
         cfg["action"] = "startswith"
-        assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 1
+        assert len(run_query(df, build_query(StringFilter("foo", "S", cfg)))) == 1
 
         cfg["raw"] = "AA"
         cfg["action"] = "startswith"
-        assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 2
+        assert len(run_query(df, build_query(StringFilter("foo", "S", cfg)))) == 2
 
         cfg["operand"] = "ne"
-        assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 3
+        assert len(run_query(df, build_query(StringFilter("foo", "S", cfg)))) == 3
 
         cfg["action"] = "endswith"
         cfg["operand"] = "="
-        assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 3
+        assert len(run_query(df, build_query(StringFilter("foo", "S", cfg)))) == 3
 
         cfg["caseSensitive"] = True
-        assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 2
+        assert len(run_query(df, build_query(StringFilter("foo", "S", cfg)))) == 2
 
         cfg["action"] = "contains"
         cfg["caseSensitive"] = False
         cfg["raw"] = "'\""
-        assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 1
+        assert len(run_query(df, build_query(StringFilter("foo", "S", cfg)))) == 1
         cfg["raw"] = "[\\"  # don't parse this as regex
-        assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 1
+        assert len(run_query(df, build_query(StringFilter("foo", "S", cfg)))) == 1
 
         cfg["raw"] = "A"
-        assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 5
+        assert len(run_query(df, build_query(StringFilter("foo", "S", cfg)))) == 5
         cfg["caseSensitive"] = True
-        assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 4
+        assert len(run_query(df, build_query(StringFilter("foo", "S", cfg)))) == 4
         cfg["raw"] = "D"
-        assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 0
+        assert (
+            len(
+                run_query(
+                    df, build_query(StringFilter("foo", "S", cfg)), ignore_empty=True
+                )
+            )
+            == 0
+        )
 
         cfg["action"] = "regex"
         cfg["raw"] = "[[A].+A$"
-        assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 2
+        assert len(run_query(df, build_query(StringFilter("foo", "S", cfg)))) == 2
 
         with pytest.raises(re.error):
             cfg["raw"] = "["
-            df.query(build_query(StringFilter("foo", "S", cfg)))
+            run_query(df, build_query(StringFilter("foo", "S", cfg)))
 
         cfg["action"] = "length"
         cfg["raw"] = "3"
-        assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 4
+        assert len(run_query(df, build_query(StringFilter("foo", "S", cfg)))) == 4
 
         df = pd.DataFrame(dict(foo=["a", "aa", "aaa", "aaaa"]))
         cfg["raw"] = "1,3"
-        assert len(df.query(build_query(StringFilter("foo", "S", cfg)))) == 3
+        assert len(run_query(df, build_query(StringFilter("foo", "S", cfg)))) == 3
