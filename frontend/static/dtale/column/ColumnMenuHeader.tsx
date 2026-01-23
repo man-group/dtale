54371609d45c226e3fad8cd5ee459f22850556eb
Andrew Schonfeld (Boston) <andrew.schonfeld@man.com>
2026-01-23 15:17:50 -0500

    https://github.com/man-group/dtale/issues/926: updates to kurtosis labeling on frontend

--- frontend/static/dtale/column/ColumnMenuHeader.tsx
+++ frontend/static/dtale/column/ColumnMenuHeader.tsx
@@ -45,13 +45,15 @@ export const SkewMsg: React.FC<{ skew: string | number }> = ({ skew }) => {
 export const kurtMsgText = (kurt?: string | number): string | undefined => {
   const kurtFloat = parseFloat(`${kurt}`);
   if (kurtFloat || kurtFloat === 0) {
-    if (kurtFloat > 3) {
+    if (kurtFloat > 2) {
       return 'leptokurtic';
-    } else if (kurtFloat === 3) {
+    } else if (kurtFloat === 0) {
       return 'mesokurtic';
-    } else {
-      // kurtFloat < 3
+    } else if (kurtFloat < -2) {
       return 'platykurtic';
+    } else {
+      // -2 < kurtosis < 0 or 0 < kurtosis < 2
+      return 'fairly mesokurtic';
     }
   }
   return undefined;
