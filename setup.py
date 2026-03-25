ce5698d152f4479fc775e0e7a6cf3f16fc5aea3d
Andrew Schonfeld (Boston) <andrew.schonfeld@man.com>
2026-03-25 16:13:30 -0400

    Add Python 3.14 CircleCI support
    - Add build_3_14 job using cimg/python:3.14-node image
    - Unpin scipy for Python >= 3.10
    - Install libtirpc-dev for rpy2 build on Python 3.14 only
    - Replace imp.reload with six.moves.reload_module (removed in Python 3.12)
    - Replace assertDictContainsSubset and assertEquals with assertEqual equivalents (removed in Python 3.12)

--- setup.py
+++ setup.py
@@ -63,6 +63,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.14",
     ],
     packages=find_packages(exclude=["tests*", "script*"]),
     package_data={
